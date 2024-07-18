2274b79971d6710ada71caab4077a45151b79049
VenkyteshJagdale <venkytesh.jagdale1997@gmail.com>
2024-07-18 20:36:51 +0530

    Comment And Cron Added.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code>{process.env.TEXT}.
+          Edit Venkytesh Jagdale{process.env.TEXT}.
         </p>
         <a
           className="App-link"
